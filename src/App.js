60b3f4742d31df145886410f4c548d239d677549
SIVASANKAR-JAYAVEL <sivasankar6104@gmail.com>
2023-03-19 00:09:21 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserAction } from './redux/actions/accountActions';
+import './styles/App.css';
+import AuthPage from './components/AuthPage/AuthPage';
+import EmailPage from './components/EmailPage/EmailPage';
 
 function App() {
+  const dispatch = useDispatch();
+  const { isLoggedIn, token } = useSelector((state) => state.userReducer);
+
+  // if a token exists, try to get the user data from the server,
+  // if this fetch has succeeded, App will Navigate us to the emails page
+  // if this fetch failed, that means the token has expired and the user needs to login
+  
+  useEffect(() => {
+    if (token) {
+      dispatch(getUserAction());
+    }
+  }, [token, dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route exact path='/'>
+            {!isLoggedIn ? <Navigate to='/account' /> : <Navigate to='/email/inbox' />}
+          </Route>
+
+          <Route exact path='/account'>
+            {!isLoggedIn ? <AuthPage /> : <Navigate to='/email/inbox' />}
+          </Route>
+
+          <Route path='/email'>
+            {/* This route has multiple sub-routes */}
+            {isLoggedIn ? <EmailPage /> : <Navigate to='/account' />}
+          </Route>
+
+          <Route exact path='/GitHub'
+            component={() => (window.location.href = 'https://github.com/sivasankar-jayavel')}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
